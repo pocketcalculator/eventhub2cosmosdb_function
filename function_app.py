50f3d84ea4cf9c0a4b69d3f14efe374ec568ebdc
Paul Sczurek <pocketcalculatorshow@gmail.com>
2024-09-22 21:41:44 -0400

    Updating DB, container names

--- function_app.py
+++ function_app.py
@@ -6,7 +6,7 @@ import logging
 app = func.FunctionApp()
 
 @app.event_hub_message_trigger(arg_name="eventhubmsg", event_hub_name="EventHubName", connection="EventHubConnectionString")
-@app.cosmos_db_output(arg_name="outputDocument", container_name="Messages", database_name="SensorReadings", connection="CosmosDBConnectionString")
+@app.cosmos_db_output(arg_name="outputDocument", container_name="Readings", database_name="SensorData", connection="CosmosDBConnectionString")
 def eventHubToCosmosDBFunction(eventhubmsg: func.EventHubEvent, outputDocument: func.Out[func.Document]):
     logging.info('Python EventHub trigger processed an event: %s', eventhubmsg.get_body().decode('utf-8'))
     
@@ -24,6 +24,7 @@ def eventHubToCosmosDBFunction(eventhubmsg: func.EventHubEvent, outputDocument: 
     # Create a document to send to CosmosDB
     document = {
         "id": extracted_data["id"],
+        "device_id": extracted_data["device_id"],
         "value": extracted_data["value"],
         "timestamp": extracted_data["timestamp"],
     }
